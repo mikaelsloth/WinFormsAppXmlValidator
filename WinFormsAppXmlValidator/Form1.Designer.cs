d60c84fe07cfc8a0aca24bf567aa2fa8bd1bc997
Mikael Sloth <mikael.sloth@tnt.dk>
2022-09-29 13:43:36 +0200

    Add project files.

---
+++ WinFormsAppXmlValidator/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace WinFormsAppXmlValidator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "XML File to Validate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "XSD Schema to use";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.SupportMultiDottedExtensions = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(456, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(456, 83);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 27);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 157);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(472, 231);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Result";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(402, 123);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 27);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Validate";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(160, 14);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(289, 23);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            this.textBox2.Leave += new System.EventHandler(this.TextBox_Leave);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(160, 87);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(289, 23);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 403);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "XmlValidator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WinFormsAppXmlValidator/Form1.cs
@@ -0,0 +1,225 @@
+﻿namespace WinFormsAppXmlValidator
+{
+    using System;
+    using System.IO;
+    using System.Windows.Forms;
+    using System.Xml;
+    using System.Xml.Linq;
+    using System.Xml.Schema;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string xmlFile;
+        private string xsdFile;
+        int nooftries = 0;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = "xml";
+            openFileDialog1.Filter = "Xml Files|*.xml";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+                textBox2.Text = openFileDialog1.FileName;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = "xsd";
+            openFileDialog1.Filter = "Xsd Schemas|*.xsd";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+                textBox3.Text = openFileDialog1.FileName;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            IValidateXml validator = new XDocumentValidation();
+            textBox1.Text = validator.Validate(xmlFile, xsdFile);
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            xmlFile = textBox2.Text;
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            xsdFile = textBox3.Text;
+        }
+
+        private void TextBox_Leave(object sender, EventArgs e)
+        {
+            if (!File.Exists(((TextBox)sender).Text))
+            {
+                _ = MessageBox.Show("Windows can not find the file specified. \r\n" + "Please check the path and filename entered");
+                nooftries++;
+                if (nooftries == 3)
+                {
+                    ((TextBox)sender).Text = "";
+                    nooftries = 0;
+                }
+            }
+            else
+            {
+                nooftries = 0;
+            }
+        }
+    }
+
+    internal interface IValidateXml
+    {
+        string Validate(string xmlfile, string xsdfile);
+    }
+
+    internal abstract class XmlValidation : IValidateXml
+    {
+        public abstract string Validate(string xmlFile, string xsdfile);
+
+        protected void XsdValidationCallback(object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Warning)
+                Result += "\r\n" + "WARNING: " + args.Message;
+            else if (args.Severity == XmlSeverityType.Error)
+                Result += "\r\n" + "ERROR: " + args.Message;
+        }
+
+        protected void XmlValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            Errors = true;
+            switch (e.Severity)
+            {
+                case XmlSeverityType.Error:
+                    Result += "\r\n" + "ERROR: " + e.Message;
+                    break;
+                case XmlSeverityType.Warning:
+                    Result += "\r\n" + "WARNING: " + e.Message;
+                    break;
+            }
+
+            Result += e.Exception != null ? "Error mesage : " + e.Exception.Message + "\r\n" : "";
+        }
+
+        protected string Result { get; set; } = string.Empty;
+        protected bool Errors { get; set; } = false;
+    }
+
+    internal class XmlDocumentValidation : XmlValidation
+    {
+        public override string Validate(string xmlFile, string xsdfile)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(xmlFile))
+                {
+                    if (!string.IsNullOrEmpty(xsdfile))
+                    {
+                        // read Xml file
+                        XmlDocument xmldoc = new();
+                        xmldoc.Load(xmlFile);
+
+                        Result += "Xml file loaded correctly.\r\n";
+
+                        // validate against xsd
+                        if (!string.IsNullOrEmpty(xsdfile))
+                        {
+                            // read schema
+                            XmlSchema myschema = XmlSchema.Read(new XmlTextReader(xsdfile), XsdValidationCallback);
+
+                            XmlSchemaSet schemas = new()
+                            {
+                                XmlResolver = new XmlUrlResolver()
+                            };
+                            _ = schemas.Add(myschema);
+                            schemas.Compile();
+
+                            Result += "Xsd file loaded correctly.\r\n";
+
+                            // validate against it
+                            xmldoc.Schemas = schemas;
+                            ValidationEventHandler eventHandler = new(XmlValidationEventHandler);
+                            xmldoc.Validate(XmlValidationEventHandler);
+                            Result += "\r\n" + "The file " + xmldoc + (Errors ? " did not validate" : " validated OK");
+                            Errors = false;
+                        }
+                    }
+                    else
+                    {
+                        _ = MessageBox.Show("Please select a xsd file to validate against");
+                    }
+                }
+                else
+                {
+                    _ = MessageBox.Show("Please select a XML file");
+                }
+            }
+            catch (Exception ex)
+            {
+                Result += "\r\n" + "An error of type " + ex.GetType().ToString() + " occured." + "\r\n" + "The error message is: " + ex.Message + "\r\nSource : " + ex.Source;
+            }
+
+            return Result;
+        }
+    }
+
+    internal class XDocumentValidation : XmlValidation
+    {
+        public override string Validate(string xmlFile, string xsdfile)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(xmlFile))
+                {
+                    if (!string.IsNullOrEmpty(xsdfile))
+                    {
+                        // read Xml file
+                        XDocument xmldoc = XDocument.Load(xmlFile);
+
+                        Result += "Xml file loaded correctly.\r\n";
+
+                        // validate against xsd
+                        if (!string.IsNullOrEmpty(xsdfile))
+                        {
+                            // read schema
+                            XmlSchema myschema = XmlSchema.Read(new XmlTextReader(xsdfile), XsdValidationCallback);
+
+                            XmlSchemaSet schemas = new()
+                            {
+                                XmlResolver = new XmlUrlResolver()
+                            };
+                            _ = schemas.Add(myschema);
+                            schemas.Compile();
+
+                            Result += "Xsd file loaded correctly.\r\n";
+
+                            // validate against it
+                            ValidationEventHandler eventHandler = new(XmlValidationEventHandler);
+                            xmldoc.Validate(schemas, eventHandler);
+                            Result += "\r\n" + "The file " + xmldoc.ToString() + (Errors ? " did not validate" : " validated OK");
+                            Errors = false;
+                        }
+                    }
+                    else
+                    {
+                        _ = MessageBox.Show("Please select a xsd file to validate against");
+                    }
+                }
+                else
+                {
+                    _ = MessageBox.Show("Please select a XML file");
+                }
+            }
+            catch (Exception ex)
+            {
+                Result += "\r\n" + "An error of type " + ex.GetType().ToString() + " occured." + "\r\n" + "The error message is: " + ex.Message + "\r\nSource : " + ex.Source;
+            }
+
+            return Result;
+        }
+    }
+}
